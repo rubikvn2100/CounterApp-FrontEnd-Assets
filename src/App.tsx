5c9672e522dc8806e754819e403d1a7ca13d32ab
Khoa Le <rubikvn2100@gmail.com>
2023-10-30 01:40:49 -0700

    Integrate NavBar with routing in "App.tsx" and add snapshot test.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import NavBar from "./components/Navigation/NavBar";
 
 const App: React.FC = () => {
-  return <h1>Hello World</h1>;
+  return (
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div>
+              <h1>Dummy Home Page</h1>
+            </div>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
--- test/App.test.tsx
+++ test/App.test.tsx
@@ -1,14 +1,8 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
 import renderer from "react-test-renderer";
 import App from "../src/App";
 
 describe("App component", () => {
-  test("renders Hello World text", () => {
-    render(<App />);
-    expect(screen.getByText("Hello World")).toBeDefined();
-  });
-
   test("matches the snapshot", () => {
     const tree = renderer.create(<App />).toJSON();
     expect(tree).toMatchSnapshot();
