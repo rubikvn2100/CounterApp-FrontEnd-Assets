284d08bd99d231da21534f78a5e1ae15f7a31c95
Khoa Le <rubikvn2100@gmail.com>
2023-10-31 23:22:32 -0700

    Add wildcard route to render Home for all paths.

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ const App: React.FC = () => {
       <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="*" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
