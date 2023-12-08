f76dc84e1203bbaa46cd4bd2e488d51cb0cfa92b
Khoa Le <rubikvn2100@gmail.com>
2023-12-08 09:03:59 -0800

    Integrate ConfigProvider into App component for global configuration management.

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,19 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import NavBar from "./components/Navigation/NavBar";
 import Home from "./pages/Home/Home";
+import { ConfigProvider } from "./contexts/ConfigContextProvider";
 
 const App: React.FC = () => {
   return (
-    <BrowserRouter>
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="*" element={<Home />} />
-      </Routes>
-    </BrowserRouter>
+    <ConfigProvider>
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </ConfigProvider>
   );
 };
 
